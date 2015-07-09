f75ce9a4a80c9e9700b6279eb51c706e1b90d67f
Kevin Beronilla <kevin.beronilla@gmail.com>
2015-07-09 17:27:28 -0500

    July 9 commit — under construction

--- js/jquery.quick-modal.js
+++ js/jquery.quick-modal.js
@@ -1,5 +1,5 @@
 /* --------------------------------------------------
-jQuery Quick Modal v0.04
+jQuery Quick Modal v0.50
 
 By Kevin Beronilla
 http://www.kevinberonilla.com
@@ -11,59 +11,95 @@ Free to use under the MIT license
 http://www.opensource.org/licenses/mit-license.php
 -------------------------------------------------- */
 (function($) { // Protect the $ alias (IIF)
-    $.fn.quickModal = function(args) {
+    $.fn.setSpeed = function(speed) {
+        this.css('-webkit-transition-duration', speed + 'ms')
+            .css('-moz-transition-duration', speed + 'ms')
+            .css('-ms-transition-duration', speed + 'ms')
+            .css('-o-transition-duration', speed + 'ms')
+            .css('transition-duration', speed + 'ms');
+    }
+    
+    $.fn.setTiming = function(timing) {
+        this.css('-webkit-transition-timing-function', timing)
+            .css('-moz-transition-timing-function', timing)
+            .css('-ms-transition-timing-function', timing)
+            .css('-o-transition-timing-function', timing)
+            .css('transition-timing-function', timing);
+    }
+    
+    $.fn.quickModal = function(args, options) {
         if (!$('#modal-background').length) $('body').append('<div id="modal-background"></div>'); // Append background; do not append if re-initialized or background already exists
         
-        if (args !== null && typeof args === 'string') { // Calling method
-            var bodyTag = $('body'),
+        if (args !== null && typeof args === 'string') { // If calling a method
+            var settings = $.extend({ // Extend the default settings established below
+                    animation: 'fade-zoom',
+                    speed: 250,
+                    timing: 'ease',
+                    closeModalSelector: '.close-modal'
+                }, options),
+                bodyTag = $('body'),
                 closeModalLink = $('.close-modal'),
-                modalBackground = $('#modal-background');
+                modalBackground = $('#modal-background'),
+                targetModal = this,
+                modal = $('.modal');
+            
+            targetModal.removeClass()
+                .addClass('modal')
+                .addClass('animation-' + settings.animation);
+            
+            if (settings.speed != 250) { // Set speeds if not equal to default
+                targetModal.setSpeed(settings.speed);
+                modalBackground.setSpeed(settings.speed);
+            }
+            
+            if (settings.timing != 'ease') { // Set timing if not equal to default
+                targetModal.setTiming(settings.timing);
+                modalBackground.setTiming(settings.timing);
+            }
             
             switch (args) {
                 case 'open':
                     bodyTag.addClass('disable-scroll');
                     modalBackground.show();
-                    this.show();
+                    targetModal.show();
                     setTimeout(function() { // Ensure elements are displayed before adding classes
                         modalBackground.addClass('visible');
-                        $(this).addClass('visible');
+                        targetModal.addClass('visible');
                     }, 25);
+                    targetModal.trigger('modalopen'); // Trigger custom 'open' event
+                    
+                    closeModalLink.unbind('click') // Unbind previously bound links to prevent closing unopened modal
+                        .click(function(e) { // Bind events based on options
+                            e.preventDefault();
+                            targetModal.quickModal('close', settings);
+                        });
+                    
+                    $(document).keyup(function(e) {
+                        if (e.keyCode == 27 && modal.is(':visible')) modal.quickModal('close', settings); // Esc
+                    });
+                    
+                    modalBackground.click(function() {
+                        modal.quickModal('close', settings);
+                    });
                     break;
                     
                 case 'close':
-                    bodyTag.removeClass('disable-scroll');
-                    modalBackground.removeClass('visible');
-                    this.removeClass('visible');
-                    setTimeout(function() {
-                        modalBackground.hide();
-                        this.hide();
-                    }, 250);
+                    if (targetModal.is(':visible')) {
+                        bodyTag.removeClass('disable-scroll');
+                        modalBackground.removeClass('visible');
+                        targetModal.removeClass('visible');
+                        setTimeout(function() {
+                            modalBackground.hide();
+                            targetModal.hide();
+                            targetModal.trigger('modalclose'); // Trigger custom 'close' event
+                        }, settings.speed);
+                    } else {
+                        console.error('Target modal is not currently visible.');
+                    }
                     break;
             }
-        } else { // Initializing plugin with options
-            var settings = $.extend({ // Extend the default settings established below
-                    modalWindowClass: '.modal',
-                    closeModalClass: '.close-modal'
-                    
-                    // ADD CALLBACK OPTIONS
-                
-                
-                }, args),
-                bodyTag = $('body'),
-                openModalLink = this,
-                modalWindow = $(settings.modalWindowClass),
-                closeModalLink = $(settings.closeModalClass),
-                modalBackground = $('#modal-background');
-            
-            function closeModal() {
-                bodyTag.removeClass('disable-scroll');
-                modalBackground.removeClass('visible');
-                modalWindow.removeClass('visible');
-                setTimeout(function() {
-                    modalBackground.hide();
-                    modalWindow.hide();
-                }, 250);
-            }
+        } else { // If initializing plugin with options
+            var openModalLink = this;
             
             openModalLink.click(function(e) {
                 e.preventDefault();
@@ -73,25 +109,8 @@ http://www.opensource.org/licenses/mit-license.php
                 
                 if (modalId === undefined) console.error('No "data-modal-id" attribute is set.');
                 
-                bodyTag.addClass('disable-scroll');
-                modalBackground.show();
-                targetModal.show();
-                setTimeout(function() { // Ensure elements are displayed before adding classes
-                    modalBackground.addClass('visible');
-                    $('#' + modalId).addClass('visible');
-                }, 25);
+                targetModal.quickModal('open', args);
             });
-            
-            closeModalLink.click(function(e) {
-                e.preventDefault();
-                closeModal();
-            });
-            
-            $(document).keyup(function(e) {
-                if (e.keyCode == 27) closeModal(); // Esc
-            });
-            
-            modalBackground.click(closeModal);
         }
         
         return this; // Return the object to enable chaining
